4e00e334d6f1ec56afb989f40861391e122c4423
Tushar kanta Nayak <85495427+tushar-kant@users.noreply.github.com>
2022-03-23 17:01:01 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const menu = document.querySelector('#mobile-menu');
 const menuLinks = document.querySelector('.navbar__menu');
 const navLogo = document.querySelector('#navbar__logo');
 
-// Display Mobile Menu
+
 const mobileMenu = () => {
   menu.classList.toggle('is-active');
   menuLinks.classList.toggle('active');
@@ -10,16 +10,15 @@ const mobileMenu = () => {
 
 menu.addEventListener('click', mobileMenu);
 
-// Show active menu when scrolling
 const highlightMenu = () => {
   const elem = document.querySelector('.highlight');
   const homeMenu = document.querySelector('#home-page');
   const aboutMenu = document.querySelector('#about-page');
   const servicesMenu = document.querySelector('#services-page');
   let scrollPos = window.scrollY;
-  // console.log(scrollPos);
 
-  // adds 'highlight' class to my menu items
+
+ 
   if (window.innerWidth > 960 && scrollPos < 600) {
     homeMenu.classList.add('highlight');
     aboutMenu.classList.remove('highlight');
